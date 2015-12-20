0919f6a1d6ed9012a064c4fd255875e3bd88ccbc
rafaelfang <cf797@mail.missouri.edu>
2015-12-20 14:04:00 -0600

    Parser

---
+++ Parser.cpp
@@ -0,0 +1,33 @@
+/*
+ * Parser.cpp
+ *
+ *  Created on: Oct 24, 2015
+ *      Author: Chao
+ */
+
+#include "Parser.h"
+
+Parser::Parser() {
+	// TODO Auto-generated ructor stub
+
+}
+
+Parser::~Parser() {
+	// TODO Auto-generated destructor stub
+}
+
+std::string& Parser::getRootName() {
+	return rootName;
+}
+
+void Parser::setRootName(std::string& rootName) {
+	this->rootName = rootName;
+}
+
+int Parser::getGlobalFlag() {
+	return globalFlag;
+}
+
+void Parser::setGlobalFlag(int globalFlag) {
+	this->globalFlag = globalFlag;
+}
---
+++ Parser.h
@@ -0,0 +1,31 @@
+/*
+ * Parser.h
+ *
+ *  Created on: Oct 24, 2015
+ *      Author: Chao
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+#include <string>
+
+class Parser {
+public:
+	Parser();
+
+	virtual ~Parser();
+
+	virtual void loadAlignmentsInfo(std::string,std::string,std::string)=0;
+	virtual void storeJsonRecords(std::string)=0;
+	virtual void storeCoordsAndPDB(std::string)=0;
+	std::string& getRootName();
+	void setRootName(std::string& rootName);
+	int getGlobalFlag() ;
+	void setGlobalFlag(int globalFlag);
+
+protected:
+	std::string rootName;
+	int globalFlag;
+};
+
+#endif /* PARSER_H_ */
