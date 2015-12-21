819b8157636bc89b1b0ae961ae5183cc63fc5a42
rafaelfang <cf797@mail.missouri.edu>
2015-12-20 23:16:37 -0600

    Driver

--- Driver.cpp
+++ Driver.cpp
@@ -11,6 +11,7 @@
 #include "Point.h"
 #include "Alignment.h"
 #include "BlastParser.h"
+#include "HHSearchParser.h"
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
@@ -18,29 +19,37 @@ int main(int argc, char* argv[]) {
 	std::string targetLocation("/home/cf797/test/casp10Seq/");
 	std::string alignmentResultLocation(
 			"/home/cf797/test/casp10_01092013_blastpgp_hhsearch_glob/");
-	std::string experimentLocation(
-			"/home/cf797/test/FullyExtendedModeling/");
+	std::string experimentLocation("/home/cf797/test/FullyExtendedModeling/");
 	std::string proteinDatabaseLocation("/home/lihongb/DATABASE/DBInfo/");
-	BlastParser blastParser(argv[2]);
-	blastParser.loadAlignmentsInfo(alignmentResultLocation,targetLocation,proteinDatabaseLocation);
-	blastParser.storeJsonRecords(experimentLocation);
-	blastParser.storeCoordsAndPDB(experimentLocation);
+	if (strcmp(argv[1], "-blast") == 0) {
+		BlastParser blastParser(argv[2]);
+		blastParser.loadAlignmentsInfo(alignmentResultLocation, targetLocation,
+				proteinDatabaseLocation);
+		blastParser.storeJsonRecords(experimentLocation);
+		blastParser.storeCoordsAndPDB(experimentLocation);
+	} else if (strcmp(argv[1], "-hhsearch") == 0) {
 
-/*
-	Alignment alignment;
+		HHSearchParser hhsearchParser(argv[2]);
+		hhsearchParser.loadAlignmentsInfo(alignmentResultLocation,
+				targetLocation, proteinDatabaseLocation);
+		hhsearchParser.storeJsonRecords(experimentLocation);
+		hhsearchParser.storeCoordsAndPDB(experimentLocation);
+	}
 
-	//load target information
-	std::string _targetName("T0681");
-	alignment.setTargetName(_targetName);
-	alignment.loadTargetInfo(targetLocation);
+	/*
+	 Alignment alignment;
 
-	//load template information
-	std::string _templateName("4EIU_A");
-	alignment.setTemplateName(_templateName);
-	alignment.loadTemplateInfo(proteinDatabaseLocation);
-*/
+	 //load target information
+	 std::string _targetName("T0681");
+	 alignment.setTargetName(_targetName);
+	 alignment.loadTargetInfo(targetLocation);
+
+	 //load template information
+	 std::string _templateName("4EIU_A");
+	 alignment.setTemplateName(_templateName);
+	 alignment.loadTemplateInfo(proteinDatabaseLocation);
+	 */
 	//print out alignment basic information
-
 	//std::cout << "alignment basic information:" << std::endl;
 	//std::cout << alignment.getTargetName() << std::endl;
 	//std::cout << "target length: "<<alignment.getTargetLength() << std::endl;
@@ -48,7 +57,7 @@ int main(int argc, char* argv[]) {
 	//std::cout << alignment.getTemplateName() << std::endl;
 	//std::cout << alignment.getTemplateSequenceInfo() << std::endl;
 	//std::cout << "template length: "<<alignment.getTemplateSequenceLength() << std::endl;
-/*
+	/*
 	 Point* templateCarbonAlphaCoords = alignment.getTemplateCarbonAlphaCoords();
 	 for (int i = 0; i < alignment.getTemplateSequenceLength(); i++) {
 	 std::cout << templateCarbonAlphaCoords[i].getX() << ","
@@ -56,23 +65,23 @@ int main(int argc, char* argv[]) {
 	 << templateCarbonAlphaCoords[i].getZ() << std::endl;
 	 }
 	 std::cout << "alignment basic information end" << std::endl;
-*/
-/*
-	alignment.setQueryStart(21);
-	std::string _queryPart("SYDNFEEPKATLTGKAIY--DGEAVGVRSG--SSEFALFQDGYAL--KGSIPVYIAQDGSY-SVSLFNGDYKLVRMGNA-PWERPSND-------TIYITVRGNTVQDIPVTPYFFV-RNVSFAKNGNKITARFTINKVVANA-----------------NMENVGIYLGTGILTDEKQKEAELKLGNTVSLD-----QENTAEIEIPSGLVNESYLYARVGVKSD-------KSSEYCYSQSIKV");
-	alignment.setQueryPart(_queryPart);
-	alignment.setQueryEnd(221);
+	 */
+	/*
+	 alignment.setQueryStart(21);
+	 std::string _queryPart("SYDNFEEPKATLTGKAIY--DGEAVGVRSG--SSEFALFQDGYAL--KGSIPVYIAQDGSY-SVSLFNGDYKLVRMGNA-PWERPSND-------TIYITVRGNTVQDIPVTPYFFV-RNVSFAKNGNKITARFTINKVVANA-----------------NMENVGIYLGTGILTDEKQKEAELKLGNTVSLD-----QENTAEIEIPSGLVNESYLYARVGVKSD-------KSSEYCYSQSIKV");
+	 alignment.setQueryPart(_queryPart);
+	 alignment.setQueryEnd(221);
 
-	alignment.setSubjectStart(1);
-	std::string _subjectPart("GIDNYEEPSETIWGEVVDEATGKRVLTDQGSEGIRVRLTELSWGDNVQHNPDFYCMMDGTFQNTKIFKGEYNVRIDGPFIPLVRENTDGTLLHDGSVNTEISGTTKVKFEVQPFLNVEFVGNPQVSNGVIKAQVRVTRGVSDEVFREKIQPMGNWKDEYLNVTDIQFFVSYSNTVGYRARDERWSSSINYEGKSFEGLLGKEVTIQSNGNVPSGRKVFVRAAARINYDTPVGSGTRRWNYSEPMEV");
-	alignment.setSubjectPart(_subjectPart);
-	alignment.setSubjectEnd(246);
+	 alignment.setSubjectStart(1);
+	 std::string _subjectPart("GIDNYEEPSETIWGEVVDEATGKRVLTDQGSEGIRVRLTELSWGDNVQHNPDFYCMMDGTFQNTKIFKGEYNVRIDGPFIPLVRENTDGTLLHDGSVNTEISGTTKVKFEVQPFLNVEFVGNPQVSNGVIKAQVRVTRGVSDEVFREKIQPMGNWKDEYLNVTDIQFFVSYSNTVGYRARDERWSSSINYEGKSFEGLLGKEVTIQSNGNVPSGRKVFVRAAARINYDTPVGSGTRRWNYSEPMEV");
+	 alignment.setSubjectPart(_subjectPart);
+	 alignment.setSubjectEnd(246);
 
-	alignment.fetchFullyExtended3DCoords();
-	std::string _methodUsed("blast");
-	alignment.setMethodUsed(_methodUsed);
-	alignment.storeInCoordsFormat(experimentLocation,1,1);
-	alignment.storeInPDBFormat(experimentLocation,1,1);
-*/
+	 alignment.fetchFullyExtended3DCoords();
+	 std::string _methodUsed("blast");
+	 alignment.setMethodUsed(_methodUsed);
+	 alignment.storeInCoordsFormat(experimentLocation,1,1);
+	 alignment.storeInPDBFormat(experimentLocation,1,1);
+	 */
 	return 0;
 }
