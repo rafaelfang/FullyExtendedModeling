383be1cf3e9d49b2f15b7fdc7bb8bf4ca5e28c7c
rafaelfang <cf797@mail.missouri.edu>
2016-02-16 10:07:19 -0600

    HHSearchParser

--- HHSearchParser.cpp
+++ HHSearchParser.cpp
@@ -1,7 +1,7 @@
 /*
  * HHSearchParser.cpp
  *
- *  Created on: Nov 14, 2015
+ *  Created on: Oct 24, 2015
  *      Author: Chao
  */
 
@@ -15,80 +15,134 @@ HHSearchParser::HHSearchParser(string _rootName) {
 	// TODO Auto-generated constructor stub
 	setRootName(_rootName);
 }
-void HHSearchParser::loadSecondaryStructureAndSolventAccessibility(
-		std::string SSandSALocation) {
 
-	std::string predictedSecondaryStructure;
-	std::string predictedSecondaryStructureConfidence;
-	std::string predictedSolventAccessibility;
+void HHSearchParser::storeRecords(string resultPosition) {
+	ofstream myfile;
+	string outputFile(resultPosition);
+	outputFile += rootName;
+	outputFile += "_hhsearch.json";
+	myfile.open((char*) outputFile.c_str());
 
-	std::string ssFile(SSandSALocation);
-	ssFile += rootName;
-	ssFile += "/QueryInfo/psipred_SS.txt";
-	FILE* fptr = fopen((char*) ssFile.c_str(), "r");
-	if (fptr == NULL) {
-		std::cout << "input file: " << ssFile << " can't open" << std::endl;
-	} else {
-		int lineLength = 5000;
-		char line[lineLength];
-		fgets(line, lineLength, fptr); //get target sequence
-
-		fgets(line, lineLength, fptr); //get predicted secondary structure;
-		std::string s(line);
-		predictedSecondaryStructure = s.erase(
-				s.find_last_not_of(" \n\r\t") + 1);
-
-		fgets(line, lineLength, fptr); //get predicted_ss_conf
-		std::string ss(line);
-		predictedSecondaryStructureConfidence = ss.erase(
-				ss.find_last_not_of(" \n\r\t") + 1);
-
-	}
-	//std::cout << predictedSecondaryStructure << std::endl;
-	//std::cout << predictedSecondaryStructureConfidence << std::endl;
-
-	std::string solventAccessibility;
-	std::string saFile(SSandSALocation);
-	saFile += rootName;
-	saFile += "/QueryInfo/sspro_SA.txt";
-	FILE* fptr2 = fopen((char*) saFile.c_str(), "r");
-	if (fptr2 == NULL) {
-		std::cout << "input file: " << saFile << " can't open" << std::endl;
-	} else {
-		int lineLength = 5000;
-		char line[lineLength];
-		fgets(line, lineLength, fptr2); //get target name
-
-		fgets(line, lineLength, fptr2); //get target sequence;
-
-		fgets(line, lineLength, fptr2); //get solvent accessibility
-		std::string s(line);
-		predictedSolventAccessibility = s.erase(
-				s.find_last_not_of(" \n\r\t") + 1);
-
-	}
-
-	//std::cout << predictedSolventAccessibility << std::endl;
+	myfile << "{\"" << rootName << "\":[" << endl;
 	for (int i = 0; i < hhsearchRecords.size(); i++) {
-		hhsearchRecords[i].setPredictedSsInfo(predictedSecondaryStructure);
-		hhsearchRecords[i].setPredictedSsConf(
-				predictedSecondaryStructureConfidence);
-		hhsearchRecords[i].setPredictedSaInfo(predictedSolventAccessibility);
+		myfile << "\t{" << endl;
+		myfile << "\t\"hitName\":\"" << hhsearchRecords[i].getTemplateName()
+				<< "\"," << endl;
+		myfile << "\t\"probab\":\"" << hhsearchRecords[i].getProbab() << "\","
+				<< endl;
+		myfile << "\t\"expect\":\"" << hhsearchRecords[i].getExpectedValue()
+				<< "\"," << endl;
+		myfile << "\t\"score\":\"" << hhsearchRecords[i].getScore() << "\","
+				<< endl;
+		myfile << "\t\"Aligned Columns\":\""
+				<< hhsearchRecords[i].getAlignedColumns() << "\"," << endl;
+		myfile << "\t\"identities\":\"" << hhsearchRecords[i].getIdentities()
+				<< "%\"," << endl;
+		myfile << "\t\"queryStart\":\"" << hhsearchRecords[i].getQueryStart()
+				<< "\"," << endl;
+		myfile << "\t\"queryPart\":\"" << hhsearchRecords[i].getQueryPart()
+				<< "\"," << endl;
+		;
+		myfile << "\t\"queryEnd\":\"" << hhsearchRecords[i].getQueryEnd()
+				<< "\"," << endl;
+		myfile << "\t\"subjectStart\":\""
+				<< hhsearchRecords[i].getSubjectStart() << "\"," << endl;
+		;
+		myfile << "\t\"subjectPart\":\"" << hhsearchRecords[i].getSubjectPart()
+				<< "\"," << endl;
+		;
+		myfile << "\t\"subjectEnd\":\"" << hhsearchRecords[i].getSubjectEnd()
+				<< "\"}";
+		if (i == hhsearchRecords.size() - 1) {
+			myfile << endl;
+		} else {
+			myfile << "," << endl;
+		}
+	}
+	myfile << "]}" << endl;
+	myfile.close();
+}
 
+void HHSearchParser::processQueryInsertedResiduesAndSubjectGapsAligned() {
+	for (int i = 0; i < hhsearchRecords.size(); i++) {
+			string queryPart = hhsearchRecords[i].getQueryPart();
+
+			string subjectPart = hhsearchRecords[i].getSubjectPart();
+
+			string newQueryPart;
+			string newSubjectPart;
+			for (int j = 0; j < queryPart.size(); j++) {
+				if (islower(queryPart[j])) {
+					newQueryPart += toupper(queryPart[j]);
+				} else {
+					newQueryPart += queryPart[j];
+				}
+
+			}
+
+			for (int j = 0; j < subjectPart.size(); j++) {
+
+				if (subjectPart[j] == '.') {
+					newSubjectPart += '-';
+				} else {
+					newSubjectPart += subjectPart[j];
+				}
+			}
+
+			hhsearchRecords[i].setQueryPart(newQueryPart);
+			hhsearchRecords[i].setSubjectPart(newSubjectPart);
+		/*cout << hhsearchRecords[i].getQueryStart()
+				<< hhsearchRecords[i].getQueryPart()
+				<< hhsearchRecords[i].getQueryEnd()
+				<< hhsearchRecords[i].getSubjectStart()
+				<< hhsearchRecords[i].getSubjectPart()
+				<< hhsearchRecords[i].getSubjectEnd() << endl;*/
 	}
 }
 
-void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
-		string targetLocation, string templateLocation) {
-	//************************************************************************
+void HHSearchParser::storeCoordinates(string experimentLocation,
+		string proteinDatabaseLocation) {
+	ofstream myfile;
+	string outputFile(experimentLocation);
+	outputFile += rootName;
+	outputFile += "/";
+	mkdir(outputFile.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
-	//************************************************************************
-	//Alignments information
+	for (int i = 0; i < hhsearchRecords.size(); i++) {
+		ofstream myfile;
+		string partFilename(outputFile);
+		partFilename += "hhsearch_";
+		char buffer[10];
+		sprintf(buffer, "%d", i);
+		partFilename += buffer;
+		partFilename += "_";
+		partFilename += hhsearchRecords[i].getTemplateName();
+		partFilename += ".txt";
+		myfile.open((char*) partFilename.c_str());
+		int queryStart = hhsearchRecords[i].getQueryStart();
+		int queryEnd = hhsearchRecords[i].getQueryEnd();
+		hhsearchRecords[i].loadTemplateInfo(proteinDatabaseLocation);
+		int queryPointsArrSize = queryEnd - queryStart + 1;
+		Point *queryPoints = (Point*) malloc(
+				sizeof(Point) * queryPointsArrSize);
+		queryPoints =
+				hhsearchRecords[i].fetchSubjectAlignedPart3DPointsForQuery();
 
+		for (int j = 0; j < queryPointsArrSize; j++) {
+			myfile << "\t" << queryPoints[j].getX() << ","
+					<< queryPoints[j].getY() << "," << queryPoints[j].getZ()
+					<< endl;
+		}
+		free(queryPoints);
+		myfile.close();
+	}
+
+}
+
+void HHSearchParser::parseFile(string hhsearchResultFileLocation) {
 	string hhsearchResultFile(hhsearchResultFileLocation);
 	hhsearchResultFile += rootName;
-	hhsearchResultFile += "/QueryInfo/query.hhr";
-	cout << hhsearchResultFile << endl;
+	hhsearchResultFile += "/query.hhr";
 	FILE* fptr = fopen((char*) hhsearchResultFile.c_str(), "r");
 	if (fptr == NULL) {
 		cout << "input file: " << hhsearchResultFile << " can't open" << endl;
@@ -131,11 +185,7 @@ void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
 				string nameLine(line);
 				string hitName = nameLine.substr(1, 6);
 				//cout << "hitName is " << hitName << endl;
-				hhsearchRecord.setTargetName(rootName);
-				hhsearchRecord.loadTargetInfo(targetLocation);
 				hhsearchRecord.setTemplateName(hitName);
-				//**********load template information here*****************
-				hhsearchRecord.loadTemplateInfo(templateLocation);
 
 			} else if (currentState == 'B') {
 
@@ -160,7 +210,7 @@ void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
 				sscanf(pch5 + 1, "%d", &identities);
 
 				hhsearchRecord.setProbab(probab);
-				hhsearchRecord.setExpect(expect);
+				hhsearchRecord.setExpectedValue(expect);
 				hhsearchRecord.setScore(score);
 				hhsearchRecord.setAlignedColumns(alignedColumns);
 				hhsearchRecord.setIdentities(identities);
@@ -171,30 +221,26 @@ void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
 				 cout << "aligned columns: " << alignedColumns << endl;
 				 cout << "identities: " << identities << endl;
 				 */
+
 			} else if (currentState == 'C') {
 
-				if (strstr(line, "ss_pred") != NULL) {
-					fgets(line, lineLength, fptr);	//skip Q ss_pred
-				}
-				//cout<<line<<endl;
-				char queryPart[200];
+				char info[14], queryPart[200];
 				int queryStart, queryEnd;
 
-				sscanf(line, "%*s %*s %d %s %d", &queryStart, queryPart,
+				sscanf(line, "%*s %s %d %s %d", info, &queryStart, queryPart,
 						&queryEnd);
-				//cout << line << endl;
 				string _queryPart(queryPart);
-				//cout << "=====" << queryStart << queryPart << queryEnd << endl;
+
 				fgets(line, lineLength, fptr); //skip q consensus
 				fgets(line, lineLength, fptr); //skip alignment
 				fgets(line, lineLength, fptr); //skip T Consensus
 				fgets(line, lineLength, fptr); //get T info
 
-				char subjectPart[200];
+				char info1[6], subjectPart[200];
 				int subjectStart, subjectEnd;
 
-				sscanf(line, "%*s %*s %d %s %d", &subjectStart, subjectPart,
-						&subjectEnd);
+				sscanf(line, "%*s %s %d %s %d", info1, &subjectStart,
+						subjectPart, &subjectEnd);
 
 				string _subjectPart(subjectPart);
 
@@ -211,16 +257,13 @@ void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
 				 cout << "subject info: " << subjectStart << subjectPart
 				 << subjectEnd << endl;
 				 */
+
 			} else if (currentState == 'D') {
 
-				if (strstr(line, "ss_pred") != NULL) {
-					fgets(line, lineLength, fptr); //skip Q ss_pred
-				}
-
-				char queryPart[200];
+				char info[14], queryPart[200];
 				int queryStart, queryEnd;
 
-				sscanf(line, "%*s %*s %d %s %d", &queryStart, queryPart,
+				sscanf(line, "%*s %s %d %s %d", info, &queryStart, queryPart,
 						&queryEnd);
 				string _queryPart(queryPart);
 				fgets(line, lineLength, fptr); //skip q consensus
@@ -228,11 +271,11 @@ void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
 				fgets(line, lineLength, fptr); //skip T Consensus
 				fgets(line, lineLength, fptr); //get T info
 
-				char subjectPart[200];
+				char info1[6], subjectPart[200];
 				int subjectStart, subjectEnd;
 
-				sscanf(line, "%*s %*s %d %s %d", &subjectStart, subjectPart,
-						&subjectEnd);
+				sscanf(line, "%*s %s %d %s %d", info1, &subjectStart,
+						subjectPart, &subjectEnd);
 				string _subjectPart(subjectPart);
 
 				string oldQueryPart = hhsearchRecord.getQueryPart();
@@ -261,102 +304,7 @@ void HHSearchParser::loadAlignmentsInfo(string hhsearchResultFileLocation,
 	}
 
 }
-void HHSearchParser::storeTrimmedString(std::string resultPosition) {
-	ofstream myfile;
-	string outputFile(resultPosition);
-	outputFile += rootName;
-	outputFile += "_hhsearch_trimmedString.txt";
-	myfile.open((char*) outputFile.c_str());
-
-	for (int i = 0; i < hhsearchRecords.size(); i++) {
-		myfile << hhsearchRecords[i].getTrimHit() << endl;
-
-	}
-	myfile << endl;
-	myfile.close();
-}
-void HHSearchParser::storeJsonRecords(string resultPosition) {
-	ofstream myfile;
-	string outputFile(resultPosition);
-	outputFile += rootName;
-	outputFile += "_hhsearch.json";
-	myfile.open((char*) outputFile.c_str());
-
-	myfile << "{\"" << rootName << "\":[" << endl;
-	for (int i = 0; i < hhsearchRecords.size(); i++) {
-		myfile << "\t{" << endl;
-		myfile << "\t\"targetName\":\"" << hhsearchRecords[i].getTargetName()
-				<< "\"," << endl;
-		myfile << "\t\"targetFullSequence\":\""
-				<< hhsearchRecords[i].getTargetSequence() << "\"," << endl;
-		myfile << "\t\"targetLength\":\""
-				<< hhsearchRecords[i].getTargetLength() << "\"," << endl;
-		myfile << "\t\"templateName\":\""
-				<< hhsearchRecords[i].getTemplateName() << "\"," << endl;
-		myfile << "\t\"templateReferenceSequenceInfo\":\""
-				<< hhsearchRecords[i].getTemplateReferenceSequenceInfo()
-				<< "\"," << endl;
-		myfile << "\t\"templateRealSequenceInfo\":\""
-				<< hhsearchRecords[i].getTemplateRealSequenceInfo() << "\","
-				<< endl;
-		myfile << "\t\"templateTrueSecondaryStructure\":\""
-				<< hhsearchRecords[i].getTemplateTrueSecondaryStructure()
-				<< "\"," << endl;
-		myfile << "\t\"templatePredictedSecondaryStructure\":\""
-				<< hhsearchRecords[i].getPredictedSsInfo() << "\"," << endl;
-		myfile << "\t\"templatePredictedSecondaryStrucutreConfidence\":\""
-				<< hhsearchRecords[i].getPredictedSsConf() << "\"," << endl;
-		myfile << "\t\"templatePredictedSolventAccessibility\":\""
-				<< hhsearchRecords[i].getPredictedSaInfo() << "\"," << endl;
-		myfile << "\t\"templateSequenceLength\":\""
-				<< hhsearchRecords[i].getTemplateSequenceLength() << "\","
-				<< endl;
-
-		myfile << "\t\"probab\":\"" << hhsearchRecords[i].getProbab() << "\","
-				<< endl;
-		myfile << "\t\"expect\":\"" << hhsearchRecords[i].getExpect() << "\","
-				<< endl;
-		myfile << "\t\"score\":\"" << hhsearchRecords[i].getScore() << "\","
-				<< endl;
-		myfile << "\t\"Aligned Columns\":\""
-				<< hhsearchRecords[i].getAlignedColumns() << "\"," << endl;
-		myfile << "\t\"identities\":\"" << hhsearchRecords[i].getIdentities()
-				<< "%\"," << endl;
-		myfile << "\t\"queryStart\":\"" << hhsearchRecords[i].getQueryStart()
-				<< "\"," << endl;
-		myfile << "\t\"queryPart\":\"" << hhsearchRecords[i].getQueryPart()
-				<< "\"," << endl;
-
-		myfile << "\t\"queryEnd\":\"" << hhsearchRecords[i].getQueryEnd()
-				<< "\"," << endl;
-		myfile << "\t\"subjectStart\":\""
-				<< hhsearchRecords[i].getSubjectStart() << "\"," << endl;
-
-		myfile << "\t\"subjectPart\":\"" << hhsearchRecords[i].getSubjectPart()
-				<< "\"," << endl;
-
-		myfile << "\t\"subjectEnd\":\"" << hhsearchRecords[i].getSubjectEnd()
-				<< "\"}";
-
-		if (i == hhsearchRecords.size() - 1) {
-			myfile << endl;
-		} else {
-			myfile << "," << endl;
-		}
-	}
-	myfile << "]}" << endl;
-	myfile.close();
-}
-void HHSearchParser::storeCoordsAndPDB(string experimentLocation) {
-	for (int i = 0; i < hhsearchRecords.size(); i++) {
-		hhsearchRecords[i].fetchFullyExtended3DCoords();
-		hhsearchRecords[i].fetchTrimHit();
-		std::string _methodUsed("hhsearch");
-		hhsearchRecords[i].setMethodUsed(_methodUsed);
-		hhsearchRecords[i].storeInCoordsFormat(experimentLocation, 1, i);
-		hhsearchRecords[i].storeInPDBFormat(experimentLocation, 1, i);
-	}
-}
 HHSearchParser::~HHSearchParser() {
 // TODO Auto-generated destructor stub
 }
+
